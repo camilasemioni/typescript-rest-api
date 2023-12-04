6e8749858e9b4c6f7d38996eb40d66109b7adc0a
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-04 17:45:37 -0300

    Initial commit

---
+++ src/interfaces/customer.interface.ts
@@ -0,0 +1,14 @@
+import { Document } from "mongoose";
+
+export default interface ICustomer extends Document {
+    name: string;
+    cpf: string;
+    email: string;
+    cep: string;
+    uf: string;
+    city: string;
+    address: string;
+    number: string;
+    complement: string;
+    neighborhood: string;
+}
