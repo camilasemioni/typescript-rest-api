7dafcf8559b4bd3d7f76fdf8fb0ff746a6189fee
Camila Rocha Semioni <camilasemioni@gmail.com>
2023-12-06 19:07:31 -0300

    style: format code with Prettier configuration

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,38 +1,62 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import CustomerModel from '../models/customer.model';
+import customerModel from '../models/customer.model';
 import { StatusCodes } from 'http-status-codes';
 
-export const updateCustomerById = async (req: Request, res: Response) => {
+export const updateCustomer = async (req: Request, res: Response) => {
     try {
         const customerId = req.params.id;
 
         if (!mongoose.Types.ObjectId.isValid(customerId)) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid client ID' });
+            res.status(StatusCodes.BAD_REQUEST).json({
+                error: 'Invalid client ID',
+            });
+            return;
         }
 
-        const { name, cep, uf, city, address, number, complement, neighborhood } = req.body;
+        const {
+            name,
+            cep,
+            uf,
+            city,
+            address,
+            number,
+            complement,
+            neighborhood,
+        } = req.body;
 
-        const existingCustomer = await CustomerModel.findById(customerId);
+        const existingCustomer =
+            await customerModel.findById(customerId);
+        console.log(existingCustomer);
 
         if (!existingCustomer) {
-            return res.status(StatusCodes.NOT_FOUND).json({ error: 'Customer not found' });
+            res.status(StatusCodes.NOT_FOUND).json({
+                error: 'Customer not found',
+            });
+            return;
         }
 
         existingCustomer.name = name || existingCustomer.name;
         existingCustomer.cep = cep || existingCustomer.cep;
         existingCustomer.uf = uf || existingCustomer.uf;
         existingCustomer.city = city || existingCustomer.city;
-        existingCustomer.address = address || existingCustomer.address;
+        existingCustomer.address =
+            address || existingCustomer.address;
         existingCustomer.number = number || existingCustomer.number;
-        existingCustomer.complement = complement || existingCustomer.complement;
-        existingCustomer.neighborhood = neighborhood || existingCustomer.neighborhood;
+        existingCustomer.complement =
+            complement || existingCustomer.complement;
+        existingCustomer.neighborhood =
+            neighborhood || existingCustomer.neighborhood;
 
         await existingCustomer.save();
 
-        res.status(StatusCodes.OK).json({ message: 'Customer updated successfully' });
+        res.status(StatusCodes.OK).json({
+            message: 'Customer updated successfully',
+        });
     } catch (error) {
         console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: 'Internal Server Error',
+        });
     }
 };
