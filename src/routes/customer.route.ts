62bf79acc00f4b5782fed53a356f22ba0cb34a26
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-06 20:34:34 -0300

    chore: temporarily remove get single customer route

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
 
-import {
-    getAllCustomers,
-    getSingleCustomer,
-} from '../controllers/customer.controller';
+import { getAllCustomers } from '../controllers/customer.controller';
 
 const router = Router();
 
 router.route('/').get(getAllCustomers);
-router.route('/:id').get(getSingleCustomer);
 
 export default router;
