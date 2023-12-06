be315a9805098b0daf610b08bf3854b7bf7f7a5b
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-06 15:26:35 -0300

    feat: add filtering, sorting and pagination to getAllCustomers

--- src/controllers/customer.controller.ts
+++ src/controllers/customer.controller.ts
@@ -1,19 +1,81 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import customerModel from '../models/customer.model';
+import Queries from '../interfaces/queries.interface';
 
 export const getAllCustomers = async (_: Request, res: Response) => {
-    const customerList = await customerModel
-        .find({})
-        .select('-password');
+    const { sort, fields } = _.query;
 
-    if (customerList.length === 0) {
-        return res
-            .status(StatusCodes.NOT_FOUND)
-            .json({ code: 404, message: 'Not Found' });
+    const fieldNames = [
+        'name',
+        'cpf',
+        'birthday',
+        'email',
+        'cep',
+        'uf',
+        'city',
+        'address',
+        'number',
+        'complement',
+        'neighborhood',
+    ];
+    const caseInsensitiveFieldNames = [
+        'name',
+        'email',
+        'city',
+        'address',
+        'complement',
+        'neighborhood',
+    ];
+
+    const queryObject: Queries = {};
+
+    fieldNames.forEach((field) => {
+        if (_.query[field]) {
+            queryObject[field] = {
+                $regex: _.query[field] as string,
+            };
+            if (caseInsensitiveFieldNames.includes(field)) {
+                queryObject[field].$options = 'i';
+            }
+        }
+    });
+
+    let result = customerModel.find(queryObject);
+    const count = await result.countDocuments();
+    if (count === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json({});
     }
 
-    res.status(StatusCodes.OK).json(customerList);
+    if (sort) {
+        const sortList = (sort as string).split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('name');
+    }
+
+    if (fields) {
+        const fieldsList = (fields as string).split(',').join(' ');
+
+        if (fieldsList.includes('password')) {
+            return res.status(StatusCodes.BAD_REQUEST).json({});
+        }
+
+        result = result.select(fieldsList);
+    }
+
+    const page = +_.query.page! || 1;
+    const limit = +_.query.limit! || 5;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const customerList = await result;
+
+    res.status(StatusCodes.OK).json({
+        nbHits: customerList.length,
+        customerList,
+    });
 };
 
 export const getSingleCustomer = async (
