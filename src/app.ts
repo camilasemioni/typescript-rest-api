bca290149f59f315a1496ef4678b5ae866d45b96
Camila Rocha Semioni <camilasemioni@gmail.com>
2023-12-06 19:10:34 -0300

    fix: import router

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import 'dotenv/config';
 import connectDB from './database/connect.database';
+import router from './routes/customer.routes';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/v1', router);
 
 const start = async () => {
     try {
