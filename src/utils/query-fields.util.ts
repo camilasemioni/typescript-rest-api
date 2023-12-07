6266613efacac086bb439e80703f34a3a7912b98
Guilherme Frederico de Carvalho Eisenlohr <geisenlohr@gmail.com>
2023-12-07 15:27:43 -0300

    feat: modularize get customers controller

---
+++ src/utils/query-fields.util.ts
@@ -0,0 +1,21 @@
+export const fieldNames = [
+    'name',
+    'cpf',
+    'birthday',
+    'email',
+    'cep',
+    'uf',
+    'city',
+    'address',
+    'number',
+    'complement',
+    'neighborhood',
+];
+export const caseInsensitiveFieldNames = [
+    'name',
+    'email',
+    'city',
+    'address',
+    'complement',
+    'neighborhood',
+];
