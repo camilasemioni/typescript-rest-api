cad2984aaed2e8af60a4dd0e37a476197f646ac6
Guilherme Frederico de Carvalho Eisenlohr <geisenlohr@gmail.com>
2023-12-07 16:01:56 -0300

    feat: change import

--- src/controllers/delete-customer.controller.ts
+++ src/controllers/delete-customer.controller.ts
@@ -1,25 +1,23 @@
 import { Request, Response } from 'express';
-import mongoose from 'mongoose';
 import CustomerModel from '../models/customer.model';
 import { StatusCodes } from 'http-status-codes';
+import errorHandler from '../utils/error-handler.util';
+import NotFoundError from '../errors/not-found.error';
 
-export const deleteCustomer = async (
-    req: Request,
-    res: Response,
-) => {
+export const deleteCustomer = async (req: Request, res: Response) => {
     try {
         const customerId = req.params.id;
 
-        if (!mongoose.Types.ObjectId.isValid(customerId)) {
-            return res
-                .status(StatusCodes.BAD_REQUEST)
-                .json({ error: 'invalid client ID' });
+        const deletedCustomer =
+            await CustomerModel.findByIdAndDelete(customerId);
+
+        if (!deletedCustomer) {
+            throw new NotFoundError('Customer not found');
         }
-
-        await CustomerModel.findByIdAndDelete(customerId);
-
-        res.status(StatusCodes.NO_CONTENT).json({ message: 'success' });
+        res.status(StatusCodes.NO_CONTENT).json({
+            message: 'success',
+        });
     } catch (error) {
-        console.log(error);
+        errorHandler(error, res);
     }
 };
