6180be4ac92a1ce79ff184f9cafe8c965fe90629
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-07 17:20:39 -0300

    feat: add automatic CPF formatting

--- src/controllers/post-customer.controller.ts
+++ src/controllers/post-customer.controller.ts
@@ -8,11 +8,15 @@ import { StatusCodes } from 'http-status-codes';
 import errorHandler from '../utils/error-handler.util';
 import { Request, Response } from 'express';
 import { hash } from 'bcrypt';
+import formatCPF from '../utils/format-cpf';
 
 export const createCustomer = async (req: Request, res: Response) => {
     try {
         const payload = req.body;
         const cepPayload = payload.cep.replace(/[^0-9]/g, '');
+        const cpfPayload = payload.cpf.replace(/[^0-9]/g, '');
+        const formattedCPF = formatCPF(cpfPayload);
+        payload.cpf = formattedCPF;
         formatViaCep(cepPayload);
         const addressUrl = `https://viacep.com.br/ws/${cepPayload}/json`;
         const viaCepResponse = (await axios.get(addressUrl)).data;
