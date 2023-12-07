81942651e5d333498b0ad922f5ffa75865d5669f
Camila Rocha Semioni <camilasemioni@gmail.com>
2023-12-07 17:11:24 -0300

    refactor: extend routes to include update functionality

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -4,10 +4,14 @@ import {
 } from '../controllers/get-customer.controller';
 import { createCustomer } from '../controllers/post-customer.controller';
 import { Router } from 'express';
+import { updateCustomer } from '../controllers/update-customer.controller';
 
 const router = Router();
 
 router.route('/client').get(getAllCustomers).post(createCustomer);
-router.route('/client/:id').get(getSingleCustomer);
+router
+    .route('/client/:id')
+    .get(getSingleCustomer)
+    .put(updateCustomer);
 
 export default router;
