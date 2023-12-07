dd0e2721f72dcc9a1b00a66beb9c87b46809b1bc
Bruno Ortigosa Cruz <97561024+bruno-ortigosa-cruz@users.noreply.github.com>
2023-12-07 15:59:24 -0300

    Revert "feat: password hashing algorithm"

--- src/routes/customer.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createClient } from '../controllers/customer.controller';
-
-const router = express.Router();
-
-router.route('/client').post(createClient);
-
-export default router;
