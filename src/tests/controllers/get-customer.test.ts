43a977881904c0ee4fa97948ae6adefeda47431c
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-08 16:20:23 -0300

    test: implement query testing to GET route

--- src/tests/controllers/get-customer.test.ts
+++ src/tests/controllers/get-customer.test.ts
@@ -3,13 +3,23 @@ import { app, server } from '../../app';
 import CustomerModel from '../../models/customer.model';
 import mongoose from 'mongoose';
 
-jest.mock('../../models/customer.model', () => ({
-    countDocuments: jest.fn(),
-    select: jest.fn(),
-    find: jest.fn(),
-}));
+jest.mock('../../models/customer.model', () => {
+    return {
+        countDocuments: jest.fn(),
+        find: jest.fn().mockReturnValue({
+            skip: jest.fn().mockReturnThis(),
+            sort: jest.fn().mockReturnThis(),
+            select: jest.fn().mockReturnThis(),
+            limit: jest.fn().mockReturnThis(),
+        }),
+    };
+});
 
 describe('getAllCustomers()', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     test('should return an empty json if the collection is empty', async () => {
         (
             CustomerModel.countDocuments as jest.Mock
@@ -28,19 +38,44 @@ describe('getAllCustomers()', () => {
         ];
 
         (CustomerModel.find as jest.Mock).mockReturnValue({
-            skip: jest.fn().mockReturnThis(),
-            sort: jest.fn().mockReturnThis(),
+            ...CustomerModel.find(),
             limit: jest.fn().mockImplementation(() => customerList),
         });
 
         const result = await request(app).get('/api/v1/client');
 
-        console.log(result.body);
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual({
             nbHits: customerList.length,
             customerList,
         });
+    });
+
+    test('should throw an error when queried for password', async () => {
+        const result = await request(app).get(
+            '/api/v1/client?fields=password',
+        );
+
+        expect(result.statusCode).toEqual(400);
+        expect(result.body.message).toEqual('Password access denied');
+    });
+
+    test('should throw an error when fields query is not valid', async () => {
+        const result = await request(app).get(
+            '/api/v1/client?fields=cbf',
+        );
+
+        expect(result.statusCode).toEqual(400);
+        expect(result.body.message).toEqual('Invalid query');
+    });
+
+    test('should throw an error when query name is not valid', async () => {
+        const result = await request(app).get(
+            '/api/v1/client?invalid=yes',
+        );
+
+        expect(result.statusCode).toEqual(400);
+        expect(result.body.message).toEqual('Invalid query');
     });
 
     afterAll((done) => {
