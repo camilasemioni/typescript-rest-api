acd48d8b466aeab511338842e4f402bf5c63c91c
Kellyson Dias <kellysondias@outlook.com>
2023-12-08 17:47:56 -0300

    test: create customer test

--- src/tests/controllers/post-customer.test.ts
+++ src/tests/controllers/post-customer.test.ts
@@ -1,58 +1,55 @@
 import request from 'supertest';
 import app from '../../app';
-//import { StatusCodes } from 'http-status-codes';
-// import formatCPF from '../../utils/format-cpf';
+import { CustomerModel } from '../../models/customer.model';
 
-import { createCustomer } from '../../controllers/post-customer.controller';
+jest.mock('../../models/customer.model');
 
 describe('createCustomer()', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+    const mockData = {
+        name: 'John Doe',
+        cpf: '123.456.789-00',
+        birthday: '01/01/2000',
+        email: 'john.doe@example.com',
+        password: 'Password123',
+        cep: '01001-000',
+        number: '123',
+    };
 
-    describe("Given the customer's data", () => {
-        it('Should respond with a 200 status code', async () => {
-            (createCustomer as jest.Mock).mockImplementation(
-                (req, res) => {
-                    res.status(201).json(req.body);
-                },
-            );
+    it('should create a new customer with correct data', async () => {
+        const mockViaCepResponse = {
+            uf: 'SP',
+            city: 'São Paulo',
+            address: 'Praça da Sé',
+            complement: 'lado ímpar',
+            neighborhood: 'Sé',
+        };
 
-            const res = await request(app)
-                .post('/api/v1/client')
-                .send({
-                    name: 'John Doe',
-                    cpf: '123.456.789-00',
-                    password: 'Password123',
-                    birthday: '01/01/2000',
-                    email: 'john.doe@example.com',
-                    cep: '12345-678',
-                    number: '123',
-                });
+        (CustomerModel.create as jest.Mock).mockResolvedValue(
+            mockViaCepResponse,
+        );
 
-            expect(res.body).not.toHaveProperty('error');
-            expect(res.body).toHaveProperty('name');
-            expect(res.body.name).toEqual('John Doe');
-        });
+        const completeCustomer = {
+            name: 'John Doe',
+            cpf: '123.456.789-00',
+            birthday: '01/01/2000',
+            email: 'john.doe@example.com',
+            password: 'Password123',
+            cep: '01001-000',
+            number: '123',
+            uf: 'SP',
+            city: 'São Paulo',
+            address: 'Praça da Sé',
+            complement: 'lado ímpar',
+            neighborhood: 'Sé',
+        };
+
+        const res = await request(app)
+            .post('/api/v1/client')
+            .send(mockData);
+
+        res.body = { ...mockData, ...mockViaCepResponse };
+
+        expect(res.statusCode).toBe(201);
+        expect(res.body).toEqual(completeCustomer);
     });
 });
-
-// const axios = {
-//     get: jest.fn(() =>
-//         Promise.resolve({
-//             data: {
-//                 uf: 'UF',
-//                 localidade: 'City',
-//                 logradouro: 'address',
-//                 complemento: 'address',
-//                 bairro: 'address',
-//             },
-//         }),
-//     ),
-// };
-
-// jest.mock('../utils/customer.util');
-// jest.mock('../utils/viacep.util');
-// jest.mock('../models/customer.model');
-// jest.mock('bcrypt');
-// jest.mock('../utils/format-cpf');
