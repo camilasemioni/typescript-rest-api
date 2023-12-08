3c7d8e43362b2363a6e8bef455f7518550b85987
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-08 18:09:31 -0300

    refactor: remove redundant try-catch

--- src/controllers/get-customer.controller.ts
+++ src/controllers/get-customer.controller.ts
@@ -8,95 +8,86 @@ import {
 } from '../utils/query-fields.util';
 import BadRequestError from '../errors/bad-request.error';
 import NotFoundError from '../errors/not-found.error';
-import errorHandler from '../utils/error-handler.util';
 
 export const getAllCustomers = async (_: Request, res: Response) => {
-    try {
-        const { sort, fields } = _.query;
+    const { sort, fields } = _.query;
 
-        const queryObject: IQueries = {};
+    const queryObject: IQueries = {};
 
-        fieldNames.forEach((field) => {
-            if (_.query[field]) {
-                queryObject[field] = {
-                    $regex: _.query[field] as string,
-                };
-                if (caseInsensitiveFieldNames.includes(field)) {
-                    queryObject[field].$options = 'i';
-                }
-            }
-        });
-
-        const count = await CustomerModel.countDocuments();
-        if (count === 0) {
-            return res.status(StatusCodes.OK).json({});
-        }
-
-        let result = CustomerModel.find(queryObject);
-
-        if (sort) {
-            const sortList = (sort as string).split(',').join(' ');
-            result = result.sort(sortList);
-        } else {
-            result = result.sort('name');
-        }
-
-        let fieldsList = '';
-
-        if (fields) {
-            fieldsList = (fields as string).split(',').join(' ');
-
-            if (fieldsList.includes('password')) {
-                throw new BadRequestError('Password access denied');
-            }
-
-            if (!fieldNames.includes(fieldsList)) {
-                throw new BadRequestError('Invalid query');
+    fieldNames.forEach((field) => {
+        if (_.query[field]) {
+            queryObject[field] = {
+                $regex: _.query[field] as string,
+            };
+            if (caseInsensitiveFieldNames.includes(field)) {
+                queryObject[field].$options = 'i';
             }
         }
+    });
 
-        const allowedFields = ['sort', 'fields', ...fieldNames];
-        const queryKeys = Object.keys(_.query);
-        if (!queryKeys.every((key) => allowedFields.includes(key))) {
+    const count = await CustomerModel.countDocuments();
+    if (count === 0) {
+        return res.status(StatusCodes.OK).json({});
+    }
+
+    let result = CustomerModel.find(queryObject);
+
+    if (sort) {
+        const sortList = (sort as string).split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('name');
+    }
+
+    let fieldsList = '';
+
+    if (fields) {
+        fieldsList = (fields as string).split(',').join(' ');
+
+        if (fieldsList.includes('password')) {
+            throw new BadRequestError('Password access denied');
+        }
+
+        if (!fieldNames.includes(fieldsList)) {
             throw new BadRequestError('Invalid query');
         }
-
-        if (fieldsList) {
-            result = result.select(fieldsList);
-        }
-
-        const page = +_.query.page! || 1;
-        const limit = +_.query.limit! || 5;
-        const skip = (page - 1) * limit;
-
-        result = result.skip(skip).limit(limit);
-
-        const customerList = await result;
-
-        res.status(StatusCodes.OK).json({
-            nbHits: customerList.length,
-            customerList,
-        });
-    } catch (error) {
-        errorHandler(error, res);
     }
+
+    const allowedFields = ['sort', 'fields', ...fieldNames];
+    const queryKeys = Object.keys(_.query);
+    if (!queryKeys.every((key) => allowedFields.includes(key))) {
+        throw new BadRequestError('Invalid query');
+    }
+
+    if (fieldsList) {
+        result = result.select(fieldsList);
+    }
+
+    const page = +_.query.page! || 1;
+    const limit = +_.query.limit! || 5;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const customerList = await result;
+
+    res.status(StatusCodes.OK).json({
+        nbHits: customerList.length,
+        customerList,
+    });
 };
 
 export const getSingleCustomer = async (
     _: Request,
     res: Response,
 ) => {
-    try {
-        const customer = await CustomerModel.findById(_.params.id);
+    const customer = await CustomerModel.findById(_.params.id);
 
-        if (!customer) {
-            throw new NotFoundError(
-                `No customer with id '${_.params.id}'`,
-            );
-        }
-
-        res.status(StatusCodes.OK).json(customer);
-    } catch (error) {
-        errorHandler(error, res);
+    if (!customer) {
+        throw new NotFoundError(
+            `No customer with id '${_.params.id}'`,
+        );
     }
+
+    res.status(StatusCodes.OK).json(customer);
 };
