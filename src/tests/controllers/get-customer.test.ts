dce648f5f54836248fd74382e7847efeaee61d69
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-08 01:59:04 -0300

    test: update test to reflect modularization of main file

--- src/tests/controllers/get-customer.test.ts
+++ src/tests/controllers/get-customer.test.ts
@@ -1,7 +1,6 @@
 import request from 'supertest';
-import { app, server } from '../../app';
-import CustomerSchema from '../../models/customer.model';
-import mongoose from 'mongoose';
+import app from '../../app';
+import { CustomerModel } from '../../models/customer.model';
 
 jest.mock('../../models/customer.model', () => ({
     countDocuments: jest.fn(),
@@ -12,23 +11,11 @@ jest.mock('../../models/customer.model', () => ({
 describe('getAllCustomers()', () => {
     test('should return an empty json if there is no items inside the array', async () => {
         (
-            CustomerSchema.countDocuments as jest.Mock
+            CustomerModel.countDocuments as jest.Mock
         ).mockResolvedValueOnce(0);
 
         const result = await request(app).get('/api/v1/client');
 
         expect(result.statusCode).toEqual(200);
-    });
-
-    afterAll((done) => {
-        mongoose.connection.close().then(() => {
-            if (server) {
-                server.close(() => {
-                    done();
-                });
-            } else {
-                done();
-            }
-        });
     });
 });
