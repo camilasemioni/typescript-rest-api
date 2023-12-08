5523d052c55481a157138c146bf7548ddc91b557
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-07 23:29:40 -0300

    test: add more get all customers tests

--- src/tests/controllers/get-customer.test.ts
+++ src/tests/controllers/get-customer.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import { app, server } from '../../app';
-import CustomerSchema from '../../models/customer.model';
+import CustomerModel from '../../models/customer.model';
 import mongoose from 'mongoose';
 
 jest.mock('../../models/customer.model', () => ({
@@ -10,14 +10,37 @@ jest.mock('../../models/customer.model', () => ({
 }));
 
 describe('getAllCustomers()', () => {
-    test('should return an empty json if there is no items inside the array', async () => {
+    test('should return an empty json if the collection is empty', async () => {
         (
-            CustomerSchema.countDocuments as jest.Mock
+            CustomerModel.countDocuments as jest.Mock
         ).mockResolvedValueOnce(0);
 
         const result = await request(app).get('/api/v1/client');
 
         expect(result.statusCode).toEqual(200);
+        expect(result.body).toEqual({});
+    });
+
+    test('should return correct format if the collection is not empty', async () => {
+        const customerList = [
+            { name: 'Customer 1' },
+            { name: 'Customer 2' },
+        ];
+
+        (CustomerModel.find as jest.Mock).mockReturnValue({
+            skip: jest.fn().mockReturnThis(),
+            sort: jest.fn().mockReturnThis(),
+            limit: jest.fn().mockImplementation(() => customerList),
+        });
+
+        const result = await request(app).get('/api/v1/client');
+
+        console.log(result.body);
+        expect(result.statusCode).toEqual(200);
+        expect(result.body).toEqual({
+            nbHits: customerList.length,
+            customerList,
+        });
     });
 
     afterAll((done) => {
