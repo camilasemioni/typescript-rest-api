78f94e823437517e139fab25060e7d5e632d8c59
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-09 19:57:45 -0300

    refactor: move allowedQueries from controllers to utils

--- src/utils/query-fields.util.ts
+++ src/utils/query-fields.util.ts
@@ -10,6 +10,17 @@ export const fieldNames = [
     'number',
     'complement',
     'neighborhood',
+    '-name',
+    '-cpf',
+    '-birthday',
+    '-email',
+    '-cep',
+    '-uf',
+    '-city',
+    '-address',
+    '-number',
+    '-complement',
+    '-neighborhood',
 ];
 export const caseInsensitiveFieldNames = [
     'name',
@@ -18,4 +29,18 @@ export const caseInsensitiveFieldNames = [
     'address',
     'complement',
     'neighborhood',
+    '-name',
+    '-email',
+    '-city',
+    '-address',
+    '-complement',
+    '-neighborhood',
+];
+
+export const allowedQueries = [
+    'limit',
+    'page',
+    'sort',
+    'fields',
+    ...fieldNames,
 ];
