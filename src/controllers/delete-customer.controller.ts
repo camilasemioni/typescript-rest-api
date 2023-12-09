421f7482c2b3067a12630391af18327d1168dcf7
Guilherme Frederico de Carvalho Eisenlohr <geisenlohr@gmail.com>
2023-12-09 15:25:29 -0300

    chore: remove try catch

--- src/controllers/delete-customer.controller.ts
+++ src/controllers/delete-customer.controller.ts
@@ -1,24 +1,18 @@
 import { Request, Response } from 'express';
 import CustomerModel from '../models/customer.model';
 import { StatusCodes } from 'http-status-codes';
-import errorHandler from '../utils/error-handler.util';
-
 import NotFoundError from '../errors/not-found.error';
 
 export const deleteCustomer = async (req: Request, res: Response) => {
-    try {
-        const customerId = req.params.id;
+    const customerId = req.params.id;
 
-        const deletedCustomer =
-            await CustomerModel.findByIdAndDelete(customerId);
+    const deletedCustomer =
+        await CustomerModel.findByIdAndDelete(customerId);
 
-        if (!deletedCustomer) {
-            throw new NotFoundError('Customer not found');
-        }
-        res.status(StatusCodes.NO_CONTENT).json({
-            message: 'success',
-        });
-    } catch (error) {
-        errorHandler(error, res);
+    if (!deletedCustomer) {
+        throw new NotFoundError('Customer not found');
     }
+    res.status(StatusCodes.NO_CONTENT).json({
+        message: 'success',
+    });  
 };
