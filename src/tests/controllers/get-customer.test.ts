741a6d3752011e10d2ffcbe81e6fe2b23d7cfb07
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-08 21:55:20 -0300

    test: add tests for get single route

--- src/tests/controllers/get-customer.test.ts
+++ src/tests/controllers/get-customer.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import app from '../../app';
 import CustomerModel from '../../models/customer.model';
+import mongoose from 'mongoose';
 
 jest.mock('../../models/customer.model', () => {
     return {
@@ -11,6 +12,7 @@ jest.mock('../../models/customer.model', () => {
             select: jest.fn().mockReturnThis(),
             limit: jest.fn().mockReturnThis(),
         }),
+        findById: jest.fn(),
     };
 });
 
@@ -75,5 +77,68 @@ describe('getAllCustomers()', () => {
 
         expect(result.statusCode).toEqual(400);
         expect(result.body.message).toEqual('Invalid query');
+    });
+});
+
+describe('getSingleCustomer', () => {
+    function validateId(result: request.Response, userId: string) {
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            result.statusCode = 400;
+        }
+    }
+
+    test('should return the user if id is valid', async () => {
+        const user = {
+            id: '6570ba4e2c13eb5171d4fa3e',
+            name: 'Customer 1',
+        };
+
+        (CustomerModel.findById as jest.Mock).mockImplementationOnce(
+            () => user,
+        );
+
+        const result = await request(app).get(
+            `/api/v1/client/${user.id}`,
+        );
+
+        expect(result.body).toEqual(user);
+    });
+
+    test('should return a status code of 404 if user valid but not found', async () => {
+        const validUserId = '6570ba4e2c13eb5171d4fa3e';
+
+        const result = await request(app).get(
+            `/api/v1/client/${validUserId}`,
+        );
+
+        validateId(result, validUserId);
+
+        expect(result.statusCode).toEqual(404);
+    });
+
+    test('should return a status code of 400 if user is invalid', async () => {
+        const invalidUserId = 'invalid';
+
+        const result = await request(app).get(
+            `/api/v1/client/${invalidUserId}`,
+        );
+
+        validateId(result, invalidUserId);
+
+        expect(result.statusCode).toEqual(400);
+    });
+
+    test('should return a message if user is invalid', async () => {
+        const invalidUserId = 'invalid';
+
+        const result = await request(app).get(
+            `/api/v1/client/${invalidUserId}`,
+        );
+
+        validateId(result, invalidUserId);
+
+        expect(result.body.message).toEqual(
+            `No customer with id '${invalidUserId}'`,
+        );
     });
 });
