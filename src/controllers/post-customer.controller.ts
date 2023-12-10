23a5741ca58a2502524c9cd03d1a9d02e0f9440b
Thiago Alves <107894642+Thiagoalves033@users.noreply.github.com>
2023-12-09 23:49:44 -0300

    fix: remove buggy redundant validation for cpf

--- src/controllers/post-customer.controller.ts
+++ src/controllers/post-customer.controller.ts
@@ -1,21 +1,14 @@
 import { removePassword } from '../utils/customer.util';
-import { formatViaCep } from '../utils/viacep.util';
 import CustomerModel from '../models/customer.model';
 import axios from 'axios';
 import BadRequestError from '../errors/bad-request.error';
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import { hash } from 'bcrypt';
-import formatCPF from '../utils/format-cpf';
 
 export const createCustomer = async (req: Request, res: Response) => {
     const payload = req.body;
-    const cepPayload = payload.cep.replace(/[^0-9]/g, '');
-    const cpfPayload = payload.cpf.replace(/[^0-9]/g, '');
-    const formattedCPF = formatCPF(cpfPayload);
-    payload.cpf = formattedCPF;
-    formatViaCep(cepPayload);
-    const addressUrl = `https://viacep.com.br/ws/${cepPayload}/json`;
+    const addressUrl = `https://viacep.com.br/ws/${payload.cep}/json`;
     const viaCepResponse = (await axios.get(addressUrl)).data;
 
     const { cep, logradouro, complemento, bairro, localidade, uf } =
