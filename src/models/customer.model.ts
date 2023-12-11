ecd3701e29b703e3d2bcd6c3b4d33de0ecc7f2c9
Bruno Ortigosa Cruz <brunortigosa@gmail.com>
2023-12-11 17:14:20 -0300

    build: clear main branch to avoid conflicts

--- src/models/customer.model.ts
+++
@@ -1,5 +0,0 @@
-import { model } from "mongoose";
-import ICustomer from "../interfaces/customer.interface";
-import CustomerSchema from "../schemas/customer.schema";
-
-export default model<ICustomer>('Customer', CustomerSchema);
